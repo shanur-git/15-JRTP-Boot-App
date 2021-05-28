b6a4c6115a3b297201aa878c9dcf26786961407d
shanur <shanu.mohammad34@gmail.com>
2021-05-28 11:15:17 +0530

    HIS-300

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -5,13 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
-	
+	public static final String = "https://github.com/home";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-	
-
-	
+	}	
 
 }
